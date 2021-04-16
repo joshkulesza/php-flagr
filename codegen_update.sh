736df4dec6b0235480e51e99a8e13357f32b4c45
Joshua Kulesza <joshua.kulesza@healthengine.com.au>
2021-04-16 16:15:31 +0800

    main Use codegen ignore

--- codegen_update.sh
+++ codegen_update.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # regenerate
 swagger-codegen generate -i https://try-flagr.herokuapp.com/swagger.json -l php -c swagger.json -o ../
-git checkout .php_cs composer.json
 composer install
 vendor/bin/php-cs-fixer fix
 vendor/bin/phpunit
